6720cbc90aeaf081ccf15d4f51173e29fd3ab077
Hernan Verar <hernanverar@gmail.com>
2023-01-12 16:00:48 -0800

    added if, else, conditionals.
    Co-authored-by: johnedisc <christopher.johnedis@gmail.com>

--- script.js
+++ script.js
@@ -4,25 +4,35 @@ function add(num1,num2){
 function subtract(num1,num2){ 
   return num1 - num2;
 }
-function multyply(num1,num2){ 
+function multiply(num1,num2){ 
   return num1 * num2;
 }
 function divide(num1,num2){ 
   return num1 / num2;
 }
 function numberSelect(event){
-event.preventDefault();
-const Selection = document.querySelector("input[name='operator']:checked").value;
-let box1 = parseInt(document.getElementById("input1").value);
-let box2 = parseInt(document.getElementById("input2").value);
-
+  event.preventDefault();
+  const selection = document.querySelector("input[name='operator']:checked").value;
+  let box1 = parseInt(document.getElementById("input1").value);
+  let box2 = parseInt(document.getElementById("input2").value);
+  let result;
+  if (selection==="add"){
+    result = add(box1,box2);
+  } else if(selection==="subtract"){
+    result = subtract(box1,box2);
+  } else if (selection==="multiply"){ 
+    result = multiply(box1,box2);
+  } else {
+    result = divide(box1,box2);
+  }
+  document.querySelector(".result").innerHTML=result
 }
 
 
 
 
 window.addEventListener("load", function(event){
-  document.getElemntById("Calculator").addEventListener("submit", numberSelect)
+  document.getElementById("Calculator").addEventListener("submit", numberSelect)
 
 }); 
 
